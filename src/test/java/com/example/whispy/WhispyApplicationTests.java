e9b158c2c45afaf2dfc4652d09baa55c624a92e8
박태수 <taesupark0504@gmail.com>
2025-06-04 08:32:52 +0900

    chore :: test

---
+++ src/test/java/com/example/whispy/WhispyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whispy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhispyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
