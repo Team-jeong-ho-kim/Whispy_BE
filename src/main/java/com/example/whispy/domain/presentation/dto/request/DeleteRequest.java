499d2b8fe15dcda473d521e491b27f0ea66e8ee1
박태수 <taesupark0504@gmail.com>
2025-06-04 08:45:55 +0900

    feat :: DeleteRequest

---
+++ src/main/java/com/example/whispy/domain/presentation/dto/request/DeleteRequest.java
@@ -0,0 +1,13 @@
+package com.example.whispy.domain.presentation.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class DeleteRequest {
+
+    @NotNull(message = "null일 수 없습니다")
+    private String s3Url;
+}
