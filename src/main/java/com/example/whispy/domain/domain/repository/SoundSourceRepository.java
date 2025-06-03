9f5f5a6c176e8a8a8f0f63e8070421973b6404bf
박태수 <taesupark0504@gmail.com>
2025-06-04 08:44:41 +0900

    feat :: repository

---
+++ src/main/java/com/example/whispy/domain/domain/repository/SoundSourceRepository.java
@@ -0,0 +1,9 @@
+package com.example.whispy.domain.domain.repository;
+
+import com.example.whispy.domain.domain.SoundSource;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SoundSourceRepository extends CrudRepository<SoundSource, Long> {
+}
