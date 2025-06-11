af8f371cd1cee96b71a787a36cd78b2ce7ef5d92
박태수 <taesupark0504@gmail.com>
2025-06-11 14:58:37 +0900

    feat :: CustomRepository

---
+++ src/main/java/com/example/whispy/domain/domain/repository/SoundSourceRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.example.whispy.domain.domain.repository;
+
+import com.example.whispy.domain.domain.enums.Theme;
+import com.example.whispy.domain.presentation.dto.response.SoundSourceResponse;
+
+import java.util.List;
+
+public interface SoundSourceRepositoryCustom {
+    
+}
