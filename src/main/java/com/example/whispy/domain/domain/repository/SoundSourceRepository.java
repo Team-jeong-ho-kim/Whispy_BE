4fe3439672df78e07bdc679a451de19f03a8270d
박태수 <taesupark0504@gmail.com>
2025-06-11 14:57:51 +0900

    feat :: customRepository 상속

--- src/main/java/com/example/whispy/domain/domain/repository/SoundSourceRepository.java
+++ src/main/java/com/example/whispy/domain/domain/repository/SoundSourceRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SoundSourceRepository extends CrudRepository<SoundSource, Long> {
+public interface SoundSourceRepository extends CrudRepository<SoundSource, Long> , SoundSourceRepositoryCustom {
 }
