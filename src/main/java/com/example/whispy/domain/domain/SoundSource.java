d51a273c1f16eabf7041e210a1875fd4af4580ea
박태수 <taesupark0504@gmail.com>
2025-06-12 15:57:55 +0900

    feat :: createdAt 필드 추가

--- src/main/java/com/example/whispy/domain/domain/SoundSource.java
+++ src/main/java/com/example/whispy/domain/domain/SoundSource.java
@@ -2,25 +2,33 @@ package com.example.whispy.domain.domain;
 
 import com.example.whispy.domain.domain.enums.Theme;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class SoundSource {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(length = 10, nullable = false)
-    private String title;
+    private String name;
 
     @Column(nullable = false)
     private String s3Url;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Theme theme;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
 }
