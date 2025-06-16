3c36d65200b0464a3b8ed4c9a8ee670fcb244b0f
박태수 <taesupark0504@gmail.com>
2025-06-16 16:15:10 +0900

    refactor :: record -> 일반 클래스로

--- src/main/java/com/example/whispy/domain/presentation/dto/response/SoundSourceResponse.java
+++ src/main/java/com/example/whispy/domain/presentation/dto/response/SoundSourceResponse.java
@@ -1,8 +1,4 @@
 package com.example.whispy.domain.presentation.dto.response;
 
-import com.example.whispy.domain.domain.enums.Theme;
-
-import java.time.LocalDateTime;
-
-public record SoundSourceResponse(Long id, String name, Theme theme, String s3Url, LocalDateTime createdAt) {
+public class SoundSourceResponse {
 }
