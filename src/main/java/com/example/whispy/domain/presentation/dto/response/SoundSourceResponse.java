7d4c0d470e84fa25c3d24be7aff608ab69bb504b
박태수 <taesupark0504@gmail.com>
2025-06-16 16:19:12 +0900

    feat :: SoundSourceReponse 클래스

--- src/main/java/com/example/whispy/domain/presentation/dto/response/SoundSourceResponse.java
+++ src/main/java/com/example/whispy/domain/presentation/dto/response/SoundSourceResponse.java
@@ -1,4 +1,30 @@
 package com.example.whispy.domain.presentation.dto.response;
 
+import com.example.whispy.domain.domain.enums.Theme;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class SoundSourceResponse {
+
+    private Long id;
+
+    private String name;
+
+    private Theme theme;
+
+    private String s3Url;
+
+    private LocalDateTime createdAt;
+
+    @QueryProjection
+    public SoundSourceResponse(Long id, String name, Theme theme, String s3Url, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.theme = theme;
+        this.s3Url = s3Url;
+        this.createdAt = createdAt;
+    }
 }
